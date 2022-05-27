6706a6a83e17ba49de38100891929c9c7aa89e56
Swapnil Arun Meshram <41813408+SwapnilMeshram19@users.noreply.github.com>
2022-05-28 01:26:50 +0530

    .

--- components/booked.js
+++ components/booked.js
@@ -9,7 +9,7 @@ function booked(){
         <hr class="b-hr">
         <div id="guest-details">
             <h5>Guest Details</h5>
-            <div class="guest-name-div"><span>Guest Name: </span><span id="guest-name">swapnil Meshram</span></div>
+            <div class="guest-name-div"><span>Guest Name: </span><span id="guest-name">swapnil meshram</span></div>
             <div class="guest-phone-div"><span>Phone No.:</span><span id="guest-no">9595183013</span></div>
         </div>
         <hr class="b-hr">
@@ -19,7 +19,7 @@ function booked(){
             <div class="status">and will update your table status</div>
             <div class="b-date-time"><span>Date & Time</span><span id="date-time">29 may 2022 12:30 PM</span></div>
             <div class="b-total-guest"><span>Guests</span><span id="total-guest">4 Guests</span></div>
-            <div class="b-guest-names"><span>Name</span><span id="b-guest-name">Swapnil Meshram</span></div>
+            <div class="b-guest-names"><span>Name</span><span id="b-guest-name">swapnil meshram</span></div>
             <div class="b-guest-id"><span>ID</span><span id="guest-id">D016230546</span></div>
         </div class="b-hr">
         <hr>
